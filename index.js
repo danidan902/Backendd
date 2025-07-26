7800a386a22b1bc9e5c072d7d024a605bf959a83
Daniel Sheleme <danid93575@gmail.com>
2025-07-26 13:13:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 //  Middleware
 app.use(cors({
-  origin: "https://daniel-web-two.vercel.app",
+  origin: "https://danidanportfolio.vercel.app",
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type"],
   credentials: true
